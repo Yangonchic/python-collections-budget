43446a6ae33f48df8de2f89a8f8f2848f3074d63
Julie Chao <julie_chao@yahoo.com.au>
2021-02-05 15:55:28 +0800

    expenses vs budget

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -43,11 +43,11 @@ def main():
     for entry in myBudgetList:
         print(entry)
 
-    fig, ax = plt.subplots()
-    labels = ['Expenses', ' Overages', 'Budget']
+    fig,ax=plt.subplots()
+    labels = ['Expenses', 'Overages', 'Budget']
     values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
     ax.bar(labels, values, color=['green', 'red', 'blue'])
-    ax.set_title('Your total expenses vs. total Budget')
+    ax.set_title('Your total expenses vs. total budget')
     plt.show()
 
 if __name__ == "__main__":
