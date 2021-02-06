d931ef9784b21d8ed49f26ad566d2e6996603f3d
Julie Chao <julie_chao@yahoo.com.au>
2021-02-06 14:27:43 +0800

    expenses vs budget

--- budget/Expense.py
+++ budget/Expense.py
@@ -46,7 +46,10 @@ class Expenses():
                               if x.category == 'Phone' or x.category == 'Auto and Gas' or
                                 x.category  == 'Classes' or x.category == 'Utilities'or
                                 x.category == 'Mortgage'}
+
         food_expenses = {x for x in self.list
                          if x.category == 'Groceries' or x.category == 'Eating Out'}
+
         unnecessary_expenses = set(self.list) - necessary_expenses - food_expenses
-        return list(necessary_expenses)+list(food_expenses)+list(unnecessary_expenses)
+
+        return [necessary_expenses, food_expenses, unnecessary_expenses]
